201751e7f63503a704a836b1e376d915dc796043
dma <dmasaliy20@gmail.com>
2020-10-20 13:29:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/compass/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.compass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView arrow;
+    private Random random = new Random();
+    private int lastDir;
+    private boolean spin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arrow = findViewById(R.id.arrow);
+    }
+
+    public void spinArrow(View view){
+        if(!spin){
+            int newDir = random.nextInt(1500);
+
+            float pivotX = arrow.getWidth() >> 1;
+            float pivotY = arrow.getHeight() >> 1;
+
+            final Animation spiner = new RotateAnimation(lastDir, newDir, pivotX, pivotY);
+            spiner.setDuration(2200);
+            spiner.setFillAfter(true);
+            spiner.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    spin = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    spin = false;
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+
+            lastDir = newDir;
+            arrow.startAnimation(spiner);
+        }
+    }
+}
